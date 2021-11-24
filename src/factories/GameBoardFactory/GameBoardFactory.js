edef685a296ca25770dde51ecef6d781f7b8b393
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-11-24 19:33:58 +0530

    ship placement and empty attack tests passed

--- src/factories/GameBoardFactory/GameBoardFactory.js
+++ src/factories/GameBoardFactory/GameBoardFactory.js
@@ -1,10 +1,84 @@
-const GameBoardFactory = () => {
-  const gridState = Array(10)
-    .fill(0)
-    .map(() => Array(10).fill({ type: "empty", isHit: false }));
+import ShipFactory from "../ShipFactory/ShipFactory";
+import {
+  EMPTY_CELL,
+  FILLED_CELL,
+  HORIZONTAL,
+  VERTICAL,
+} from "./GameBoardFactoryUtil";
 
-  const placeShip = 0;
-  const receiveAttack = 0;
+const GameBoardFactory = () => {
+  const gridState = [...Array(10)].map(() =>
+    Array(10).fill({ type: EMPTY_CELL, isHit: false })
+  );
+  const shipsManager = [];
+
+  const isGivenRangeValid = ({ start, end }) => {
+    const iscoordInRange = (value) => value >= 0 && value < 10;
+    if (
+      !iscoordInRange(start.col) ||
+      !iscoordInRange(start.row) ||
+      !iscoordInRange(end.col) ||
+      !iscoordInRange(end.row)
+    ) {
+      return false;
+    }
+    const isVertical = start.col === end.col;
+    if (isVertical) {
+      const currCol = start.col;
+      for (let i = start.row; i <= end.row; i += 1) {
+        if (gridState[i][currCol].type === FILLED_CELL) {
+          return false;
+        }
+      }
+    } else {
+      const currRow = start.row;
+      for (let i = start.col; i <= end.col; i += 1) {
+        if (gridState[currRow][i].type === FILLED_CELL) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  const placeShip = ({ row, col, orientation }, shipObj = ShipFactory(4)) => {
+    const currShip = shipObj;
+    const currLength = currShip.length;
+    const start = { row, col };
+    let end = { row, col };
+    if (orientation === VERTICAL) {
+      end = { row: row + currLength - 1, col };
+    } else {
+      end = { row, col: col + currLength - 1 };
+    }
+    if (isGivenRangeValid({ start, end })) {
+      shipsManager.push({
+        currShip,
+        start,
+        end,
+        shipId: shipsManager.length,
+        orientation,
+      });
+      if (orientation === VERTICAL) {
+        const currCol = start.col;
+        for (let i = start.row; i <= end.row; i += 1) {
+          gridState[i][currCol] = { type: FILLED_CELL, isHit: false };
+        }
+      } else if (orientation === HORIZONTAL) {
+        const currRow = start.row;
+        for (let i = start.col; i <= end.col; i += 1) {
+          gridState[currRow][i] = { type: FILLED_CELL, isHit: false };
+        }
+      }
+    }
+  };
+
+  const receiveAttack = ({ row, col }) => {
+    const cellUnderAttack = Object.create(gridState[row][col]);
+    cellUnderAttack.isHit = true;
+    gridState[row][col] = cellUnderAttack;
+  };
+
   return {
     placeShip,
     receiveAttack,
@@ -21,10 +95,3 @@ const GameBoardFactory = () => {
 };
 
 export default GameBoardFactory;
-
-// place ship :  coordinates
-// recieve attack  : coordinates
-// status of grid
-
-// ships alive Number
-// all ships sunk
---
+++ src/factories/GameBoardFactory/GameBoardFactoryUtil.js
@@ -0,0 +1,5 @@
+const VERTICAL = "ver";
+const HORIZONTAL = "hor";
+const EMPTY_CELL = "empty";
+const FILLED_CELL = "filled";
+export { VERTICAL, HORIZONTAL, EMPTY_CELL, FILLED_CELL };
