a0a51a49f2668d8b0239ec7b107a2c2503d1eb48
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-11-25 15:24:49 +0530

    created tests for player factory function

--- src/factories/PlayerFactory/PlayerFactory.test.js
+++ src/factories/PlayerFactory/PlayerFactory.test.js
@@ -1,7 +1,89 @@
 // Create Player.
 
-//     Players can take turns playing the game by attacking the enemy Gameboard.
-//     The game is played against the computer, so make ‘computer’ players
-//     capable of making random plays.
-// The AI does not have to be smart, but it should know whether or not a given move is legal.
-// (i.e. it shouldn’t shoot the same coordinate twice).
+import { expect, test } from "@jest/globals";
+import GameBoardFactory from "../GameBoardFactory/GameBoardFactory";
+import { HORIZONTAL } from "../GameBoardFactory/GameBoardFactoryUtil";
+import ShipFactory from "../ShipFactory/ShipFactory";
+import PlayerFactory from "./PlayerFactory";
+
+test("initial board status", () => {
+  const tmpPlayer = PlayerFactory();
+  expect(tmpPlayer.attackBoardLog).toMatchObject([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("waiting for turn to Complete", () => {
+  const tmpPlayer = PlayerFactory();
+  tmpPlayer.setTurn();
+
+  expect(tmpPlayer.isTurnComplete).toBe(false);
+});
+
+test("attack on a cell with no ship", () => {
+  const tmpGameBoard = GameBoardFactory();
+
+  const tmpPlayer = PlayerFactory();
+  tmpPlayer.setTurn();
+  tmpPlayer.attackOpponentBoard({ row: 0, col: 0 }, tmpGameBoard);
+  expect(tmpPlayer.isTurnComplete).toBe(true);
+  expect(tmpPlayer.attackBoardLog).toMatchObject([
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("attack on a cell with ship", () => {
+  const tmpGameBoard = GameBoardFactory();
+  const tmpShip = ShipFactory(4);
+
+  tmpGameBoard.placeShip({ row: 0, col: 0, orientation: HORIZONTAL }, tmpShip);
+
+  const tmpPlayer = PlayerFactory();
+  tmpPlayer.setTurn();
+  tmpPlayer.attackOpponentBoard({ row: 0, col: 0 }, tmpGameBoard);
+  expect(tmpPlayer.attackBoardLog).toMatchObject([
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  tmpPlayer.setTurn();
+  tmpPlayer.attackOpponentBoard({ row: 0, col: 0 }, tmpGameBoard);
+  expect(tmpPlayer.isTurnComplete).toBe(false);
+  expect(tmpPlayer.attackBoardLog).toMatchObject([
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
