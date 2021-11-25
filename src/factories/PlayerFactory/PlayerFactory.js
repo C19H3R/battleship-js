f15c05f360d30c7ae14e8d372a03309a8af78b5c
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-11-25 15:25:26 +0530

    passed tests for player factory functions

--- src/factories/PlayerFactory/PlayerFactory.js
+++ src/factories/PlayerFactory/PlayerFactory.js
@@ -1,11 +1,25 @@
 const PlayerFactory = () => {
-  const opponentBoardStatus = [];
-  const isTurn = false;
+  const opponentBoardLog = [...Array(10)].map(() => Array(10).fill(0));
+  let isTurn = false;
+  const attackOpponentBoard = ({ row, col }, gameBoard) => {
+    if (opponentBoardLog[row][col] === 0) {
+      opponentBoardLog[row][col] = 1;
+      isTurn = false;
+      gameBoard.receiveAttack({ row, col });
+    }
+  };
   return {
-    attackBoard: 0,
-    setTurn: 0,
+    attackOpponentBoard,
+    get attackBoardLog() {
+      return opponentBoardLog;
+    },
+    setTurn: () => {
+      isTurn = true;
+    },
     get isTurnComplete() {
       return !isTurn;
     },
   };
 };
+
+export default PlayerFactory;
